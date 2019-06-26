33d31bdef0c3fea0c20260e6615217801117da32
wipro94 <52003644+wipro94@users.noreply.github.com>
2019-06-26 22:48:04 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,21 @@
+package com.example.entities;
+import java.util.List;
+public class Product
+{
+  private String id;
+  private String name;
+  private String description;
+  public Product()
+  {
+    
+  }
+  
+  public Product(String id, String name, String description)
+  {
+    super();
+    this.id=id;
+    this.name=name;
+    this.description=description;
+    this.products=products;
+    
+   }
